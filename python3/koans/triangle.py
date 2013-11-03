0028c79f81ff7fc12be14db0f0b0fd7c63b6e411
pasierb <michal.pasierbski@sooftly.com>
2013-11-03 14:05:54 +0100

    learn triangle project 1

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -17,8 +17,18 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+  """
+  Triangle analyzes the lengths of the sides of a triangle
+  (represented by a, b and c) and returns the type of triangle.
+
+  It returns:
+    'equilateral'  if all sides are equal
+    'isosceles'    if exactly 2 sides are equal
+    'scalene'      if no sides are equal
+  """
+  triangle_types = {1: 'equilateral', 2: 'isosceles', 3: 'scalene'}
+
+  return triangle_types[set((a, b, c)).__len__()]
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
